014530251dba047e063fb3db7f176dfd4b505cde
Vy Nguyen <vy1872@gmail.com>
2022-08-16 21:41:01 -0700

    made the styling of the page better

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -14,7 +14,7 @@ const parseDataNeeded = async (dataRetrieved) => {
         currentTemp: `Temp: ${data.current.temp} °F`,
         currentWind: `Wind: ${data.current.wind_speed} MPH`,
         currentHumidity: `Humidity: ${data.current.humidity}`,
-        currentUvIndex: `UV Index: ${data.current.uvi}`,
+        currentUvIndex: data.current.uvi
     }
 
     let forcast = [];
@@ -36,24 +36,45 @@ const parseDataNeeded = async (dataRetrieved) => {
 }
 
 const renderData = ({name, currentDate, currentIcon, currentTemp, currentWind, currentHumidity, currentUvIndex, forcast}) => {
+    $(`#today`).attr(`style`, `border: 2px solid black`)
     $(`#cityName`).text(`${name} ${currentDate}`);
     $(`#currentIcon`).attr(`src`, `${currentIcon}`);
     $(`#tempature`).text(`${currentTemp}`);
     $(`#wind`).text(`${currentWind}`);
     $(`#humidity`).text(`${currentHumidity}`);
-    $(`#uvIndex`).text(`${currentUvIndex}`);
+    $(`#uvIndexText`).text(`UV Index: `);
+
+    if (currentUvIndex < 3) {
+        $(`#uvIndexValue`).text(`${currentUvIndex}`).attr(`style`, `background-color: #00FF00`);
+    } else if (currentUvIndex < 6) {
+        $(`#uvIndexValue`).text(`${currentUvIndex}`).attr(`style`, `background-color: #FFFF00`);
+    } else {
+        $(`#uvIndexValue`).text(`${currentUvIndex}`).attr(`style`, `background-color: #FF0000`);
+    }
 
     cards = document.querySelectorAll(`figure`);
 
     for(i = 0; i < forcast.length; i++) {
+        cards[i].setAttribute(`style`, `background-color: blue`);
         cards[i].children[0].textContent = `${forcast[i].date}`
         cards[i].children[1].setAttribute(`src`, forcast[i].icon)
         cards[i].children[2].textContent = `${forcast[i].temp}`
         cards[i].children[3].textContent = `${forcast[i].wind}`
         cards[i].children[4].textContent = `${forcast[i].humidity}`
     }
+
+    hidden = document.querySelectorAll(`.hidden`);
+
+    if(hidden.length !== 0) {
+        for(i = 0; i < hidden.length; i++){
+            hidden[i].setAttribute(`class`, `visible`)
+        }  
+    }
 }
 
 const getAndRenderData = () => getData().then(parseDataNeeded).then(renderData)
 
+// let dataRetrieved = JSON.parse(localStorage.getItem(`tempData`));
+
+// parseDataNeeded(dataRetrieved).then(renderData);
 $("#search").on("click", getAndRenderData);
