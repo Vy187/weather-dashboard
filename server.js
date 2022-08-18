662f90011747f0e58cb6605f44d9a23551106082
Vy Nguyen <vy1872@gmail.com>
2022-08-17 20:54:02 -0700

    added cors to protect the routes

--- server.js
+++ server.js
@@ -2,12 +2,27 @@ const express = require(`express`);
 const path = require(`path`);
 const api = require(`./routes/api`);
 const app = express();
-const rateLimit = require(`express-rate-limit`);
-const limiter = rateLimit({ windowMs: 1000, max: 1 })
 const PORT = process.env.PORT || 3001;
+const rateLimit = require(`express-rate-limit`);
+const limiter = rateLimit({ windowMs: 1000, max: 1 });
+const cors = require(`cors`);
+const whitelist = [`http://localhost:3001/`];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error(`Note allowed by CORS`))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+
+app.use(express.static(`public`));
 
 app.use(limiter);
-app.use(express.static(`public`));
+app.use(cors(corsOptions))
+
 app.use(`/api`, api);
 
 app.get(`/`, (req, res) => res.sendFile(path.join(__dirname, `/public/index.html`)));
