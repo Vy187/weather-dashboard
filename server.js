1860291b080c037919ca0e81457c82e2109eac81
Vy Nguyen <vy1872@gmail.com>
2022-08-17 20:29:28 -0700

    added the rate limiter package and applied it to the server

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require(`express`);
 const path = require(`path`);
 const api = require(`./routes/api`);
 const app = express();
+const rateLimit = require(`express-rate-limit`);
+const limiter = rateLimit({ windowMs: 1000, max: 1 })
 const PORT = process.env.PORT || 3001;
 
+app.use(limiter);
+app.use(express.static(`public`));
 app.use(`/api`, api);
-
-app.use(express.static(`public`))
 
 app.get(`/`, (req, res) => res.sendFile(path.join(__dirname, `/public/index.html`)));
 
