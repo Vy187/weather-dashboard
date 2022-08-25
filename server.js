840282af2d3d6c71f3f8b8e5019955d56e373b4c
Vy Nguyen <vy1872@gmail.com>
2022-08-24 17:37:17 -0700

    just seeing if cors is the problem

--- server.js
+++ server.js
@@ -5,23 +5,23 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const rateLimit = require(`express-rate-limit`);
 const limiter = rateLimit({ windowMs: 1000, max: 1 });
-const cors = require(`cors`);
-const whitelist = [`http://localhost:${PORT}`, `https://vys-weather-dashboard.herokuapp.com/`];
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error(`Note allowed by CORS`))
-        }
-    },
-    optionsSuccessStatus: 200
-}
+// const cors = require(`cors`);
+// const whitelist = [`http://localhost:${PORT}`, `https://vys-weather-dashboard.herokuapp.com/`];
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//         if (!origin || whitelist.indexOf(origin) !== -1) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error(`Note allowed by CORS`))
+//         }
+//     },
+//     optionsSuccessStatus: 200
+// }
 
 app.use(express.static(`public`));
 
 app.use(limiter);
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
 app.use(`/api`, api);
 
